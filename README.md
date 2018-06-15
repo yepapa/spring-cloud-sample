352354a04ec31fafc43ac408e7de1e1be06d233d
Juhyoung Jeon <shuiky2000@gmail.com>
2018-06-16 00:05:10 +0900

    eureka service setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Cloud Sample
+
+# Eureka Service
+
+java10에서 tomcat 시작 시 오류
+https://stackoverflow.com/questions/43574426/how-to-resolve-java-lang-noclassdeffounderror-javax-xml-bind-jaxbexception-in-j
+https://www.concretepage.com/forum/thread?qid=531
---
+++ eureka-service/src/main/java/com/yejpapa/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,28 @@
+package com.yejpapa.eurekaservice;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServiceApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
